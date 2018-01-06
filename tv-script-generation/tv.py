57c0b25541cefc08c8172485a4e1b18295b6ceac
david howard <dmh2000@gmail.com>
2018-01-05 18:04:48 -0800

    tv script almost working

--- tv-script-generation/tv.py
+++ tv-script-generation/tv.py
@@ -224,7 +224,7 @@ def build_nn(cell, rnn_size, input_data, vocab_size, embed_dim):
     print(rnn_outputs.get_shape())
     print(final_state.get_shape())
 
-    logits = tf.contrib.layers.fully_connected(rnn_outputs, vocab_size, activation_fn=tf.sigmoid)
+    logits = tf.contrib.layers.fully_connected(rnn_outputs, vocab_size, activation_fn=None)
 
     print(logits.get_shape())
 
@@ -289,3 +289,184 @@ print(np.array(range(x * 35 + 1, x * 35 + 1 + 5)))
 DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
 """
 tests.test_get_batches(get_batches)
+
+# Number of Epochs
+num_epochs = 2
+# Batch Size
+batch_size = 128
+# RNN Size
+rnn_size = 256
+# Embedding Dimension Size
+embed_dim = 256
+# Sequence Length
+seq_length = 10
+# Learning Rate
+learning_rate = 0.001
+# Show stats for every n number of batches
+show_every_n_batches = 100
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+save_dir = './save'
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL
+"""
+from tensorflow.contrib import seq2seq
+
+train_graph = tf.Graph()
+with train_graph.as_default():
+    vocab_size = len(int_to_vocab)
+    input_text, targets, lr = get_inputs()
+    input_data_shape = tf.shape(input_text)
+    cell, initial_state = get_init_cell(input_data_shape[0], rnn_size)
+    logits, final_state = build_nn(cell, rnn_size, input_text, vocab_size, embed_dim)
+
+    # Probabilities for generating words
+    probs = tf.nn.softmax(logits, name='probs')
+
+    # Loss function
+    cost = seq2seq.sequence_loss(
+        logits,
+        targets,
+        tf.ones([input_data_shape[0], input_data_shape[1]]))
+
+    # Optimizer
+    optimizer = tf.train.AdamOptimizer(lr)
+
+    # Gradient Clipping
+    gradients = optimizer.compute_gradients(cost)
+    capped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients if grad is not None]
+    train_op = optimizer.apply_gradients(capped_gradients)
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL
+"""
+batches = get_batches(int_text, batch_size, seq_length)
+
+with tf.Session(graph=train_graph) as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for epoch_i in range(num_epochs):
+        state = sess.run(initial_state, {input_text: batches[0][0]})
+
+        for batch_i, (x, y) in enumerate(batches):
+            feed = {
+                input_text: x,
+                targets: y,
+                initial_state: state,
+                lr: learning_rate}
+            train_loss, state, _ = sess.run([cost, final_state, train_op], feed)
+
+            # Show every <show_every_n_batches> batches
+            if (epoch_i * len(batches) + batch_i) % show_every_n_batches == 0:
+                print('Epoch {:>3} Batch {:>4}/{}   train_loss = {:.3f}'.format(
+                    epoch_i,
+                    batch_i,
+                    len(batches),
+                    train_loss))
+
+    # Save Model
+    saver = tf.train.Saver()
+    saver.save(sess, save_dir)
+    print('Model Trained and Saved')
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL
+"""
+# Save parameters for checkpoint
+helper.save_params((seq_length, save_dir))
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL
+"""
+import tensorflow as tf
+import numpy as np
+import helper
+import problem_unittests as tests
+
+_, vocab_to_int, int_to_vocab, token_dict = helper.load_preprocess()
+seq_length, load_dir = helper.load_params()
+
+def get_tensors(loaded_graph):
+    """
+    Get input, initial state, final state, and probabilities tensor from <loaded_graph>
+    :param loaded_graph: TensorFlow graph loaded from file
+    :return: Tuple (InputTensor, InitialStateTensor, FinalStateTensor, ProbsTensor)
+    """
+    # TODO: Implement Function
+    inp    = loaded_graph.get_tensor_by_name("input:0")
+    istate = loaded_graph.get_tensor_by_name("initial_state:0")
+    fstate = loaded_graph.get_tensor_by_name("final_state:0")
+    probs  = loaded_graph.get_tensor_by_name("probs:0")
+    return inp,istate,fstate,probs
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+tests.test_get_tensors(get_tensors)
+
+def pick_word(probabilities, int_to_vocab):
+    """
+    Pick the next word in the generated text
+    :param probabilities: Probabilites of the next word
+    :param int_to_vocab: Dictionary of word ids as the keys and words as the values
+    :return: String of the predicted word
+    """
+    # TODO: Implement Function
+    index = np.random.choice(len(int_to_vocab.values()), p=probabilities)
+    print(index)
+    return int_to_vocab[index]
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+tests.test_pick_word(pick_word)
+
+gen_length = 20
+# homer_simpson, moe_szyslak, or Barney_Gumble
+prime_word = 'moe_szyslak'
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+loaded_graph = tf.Graph()
+with tf.Session(graph=loaded_graph) as sess:
+    # Load saved model
+    loader = tf.train.import_meta_graph(load_dir + '.meta')
+    loader.restore(sess, load_dir)
+
+    # Get Tensors from loaded model
+    input_text, initial_state, final_state, probs = get_tensors(loaded_graph)
+
+    # Sentences generation setup
+    gen_sentences = [prime_word + ':']
+    prev_state = sess.run(initial_state, {input_text: np.array([[1]])})
+
+    # Generate sentences
+    for n in range(gen_length):
+        # Dynamic Input
+        dyn_input = [[vocab_to_int[word] for word in gen_sentences[-seq_length:]]]
+        dyn_seq_length = len(dyn_input[0])
+
+        # Get Prediction
+        probabilities, prev_state = sess.run(
+            [probs, final_state],
+            {input_text: dyn_input, initial_state: prev_state})
+
+        pred_word = pick_word(probabilities[dyn_seq_length - 1], int_to_vocab)
+
+        gen_sentences.append(pred_word)
+
+    # Remove tokens
+    tv_script = ' '.join(gen_sentences)
+    for key, token in token_dict.items():
+        ending = ' ' if key in ['\n', '(', '"'] else ''
+        tv_script = tv_script.replace(' ' + token.lower(), key)
+    tv_script = tv_script.replace('\n ', '\n')
+    tv_script = tv_script.replace('( ', '(')
+
+    print(tv_script)
