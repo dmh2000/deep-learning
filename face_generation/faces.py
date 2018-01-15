9cc4bb468bbe522ffbe55ab7a0388d7e61cb98fa
david howard <dmh2000@gmail.com>
2018-01-15 09:52:28 -0800

    model is training , uploading for google cloud instance

--- face_generation/faces.py
+++ face_generation/faces.py
@@ -136,41 +136,49 @@ tests.test_discriminator(discriminator, tf)
 
 
 # =================================================
-# Discriminator
+# Generator
 # =================================================
 
-
-def generator(z, out_channel_dim, is_train=True, alpha=0.2, reuse=False):
+def generator(z, out_channel_dim, is_train=True, alpha=0.2):
     """
     Create the generator network
     :param z: Input z
     :param out_channel_dim: The number of channels in the output image
     :param is_train: Boolean if generator is being used for training
     :param alpha : leaky relu rate
-    :param reuse : boolean if the weights are reused
     :return: The tensor output of the generator
     """
+    s = 32
+
+    # set reuse condition
+    if is_train:
+        # don't reuse when training
+        reuse = False
+    else:
+        # reuse when inferring
+        reuse = True
+
     with tf.variable_scope('generator', reuse=reuse):
         # First fully connected layer
-        x1 = tf.layers.dense(z, 4 * 4 * 512)
+        x1 = tf.layers.dense(z, 7 * 7 * (32 * s))
         # Reshape it to start the convolutional stack
-        x1 = tf.reshape(x1, (-1, 4, 4, 512))
+        x1 = tf.reshape(x1, (-1, 7, 7, (32 * s)))
         x1 = tf.layers.batch_normalization(x1, training=is_train)
         x1 = tf.maximum(alpha * x1, x1)
         # 512
 
-        x2 = tf.layers.conv2d_transpose(x1, 256, 5, strides=2, padding='same')
+        x2 = tf.layers.conv2d_transpose(x1, (14 * s), 5, strides=2, padding='same')
         x2 = tf.layers.batch_normalization(x2, training=is_train)
         x2 = tf.maximum(alpha * x2, x2)
         # 256
 
-        x3 = tf.layers.conv2d_transpose(x2, 128, 5, strides=2, padding='same')
+        x3 = tf.layers.conv2d_transpose(x2, (7 * s), 5, strides=2, padding='same')
         x3 = tf.layers.batch_normalization(x3, training=is_train)
         x3 = tf.maximum(alpha * x3, x3)
         # 128
 
-        # Output layer, 32x32x3
-        logits = tf.layers.conv2d_transpose(x3, out_channel_dim, 5, strides=2, padding='same')
+        # Output layer, 28x28x5
+        logits = tf.layers.conv2d_transpose(x3, out_channel_dim, 5, strides=1, padding='same')
 
         out = tf.tanh(logits)
     return out
@@ -180,3 +188,175 @@ def generator(z, out_channel_dim, is_train=True, alpha=0.2, reuse=False):
 DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
 """
 tests.test_generator(generator, tf)
+
+
+# =================================================
+# Model Loss
+# =================================================
+
+def model_loss(input_real, input_z, output_dim, alpha=0.2):
+    """
+    Get the loss for the discriminator and generator
+    :param input_real: Images from the real dataset
+    :param input_z: Z input
+    :param out_channel_dim: The number of channels in the output image
+    :return: A tuple of (discriminator loss, generator loss)
+    """
+    g_model = generator(input_z, output_dim, alpha=alpha)
+    d_model_real, d_logits_real = discriminator(input_real, alpha=alpha)
+    d_model_fake, d_logits_fake = discriminator(g_model, reuse=True, alpha=alpha)
+
+    d_loss_real = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_real, labels=tf.ones_like(d_model_real)))
+    d_loss_fake = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, labels=tf.zeros_like(d_model_fake)))
+    g_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, labels=tf.ones_like(d_model_fake)))
+
+    d_loss = d_loss_real + d_loss_fake
+
+    return d_loss, g_loss
+
+
+# =================================================
+# Optimizer
+# =================================================
+
+def model_opt(d_loss, g_loss, learning_rate, beta1):
+    """
+    Get optimization operations
+    :param d_loss: Discriminator loss Tensor
+    :param g_loss: Generator loss Tensor
+    :param learning_rate: Learning Rate Placeholder
+    :param beta1: The exponential decay rate for the 1st moment in the optimizer
+    :return: A tuple of (discriminator training operation, generator training operation)
+    """
+    # Get weights and bias to update
+    t_vars = tf.trainable_variables()
+    d_vars = [var for var in t_vars if var.name.startswith('discriminator')]
+    g_vars = [var for var in t_vars if var.name.startswith('generator')]
+
+    # Optimize
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        d_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(d_loss, var_list=d_vars)
+        g_train_opt = tf.train.AdamOptimizer(learning_rate, beta1=beta1).minimize(g_loss, var_list=g_vars)
+
+    return d_train_opt, g_train_opt
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+tests.test_model_opt(model_opt, tf)
+
+# =================================================
+# Show Output
+# =================================================
+
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL
+"""
+import numpy as np
+
+
+def show_generator_output(sess, n_images, input_z, out_channel_dim, image_mode):
+    """
+    Show example output for the generator
+    :param sess: TensorFlow session
+    :param n_images: Number of Images to display
+    :param input_z: Input Z Tensor
+    :param out_channel_dim: The number of channels in the output image
+    :param image_mode: The mode to use for images ("RGB" or "L")
+    """
+    cmap = None if image_mode == 'RGB' else 'gray'
+    z_dim = input_z.get_shape().as_list()[-1]
+    example_z = np.random.uniform(-1, 1, size=[n_images, z_dim])
+
+    samples = sess.run(
+        generator(input_z, out_channel_dim, False),
+        feed_dict={input_z: example_z})
+
+    images_grid = helper.images_square_grid(samples, image_mode)
+    pyplot.imshow(images_grid, cmap=cmap)
+    pyplot.show()
+
+
+# =================================================
+# Train MNIST
+# =================================================
+
+
+def train(epoch_count, batch_size, z_dim, learning_rate, beta1, get_batches, data_shape, data_image_mode):
+    """
+    Train the GAN
+    model_inputs(image_width, image_height, image_channels, z_dim)
+    model_loss(input_real, input_z, out_channel_dim)
+    model_opt(d_loss, g_loss, learning_rate, beta1)
+    :param epoch_count: Number of epochs
+    :param batch_size: Batch Size
+    :param z_dim: Z dimension
+    :param learning_rate: Learning Rate
+    :param beta1: The exponential decay rate for the 1st moment in the optimizer
+    :param get_batches: Function to get batches
+    :param data_shape: Shape of the data
+    :param data_image_mode: The image mode to use for images ("RGB" or "L")
+    """
+    # TODO: Build Model
+    tf.reset_default_graph()
+
+    inputs_real, inputs_z, lr = model_inputs(data_shape[1], data_shape[2], data_shape[3], z_dim)
+
+    if not learning_rate:
+        learning_rate = lr
+
+    d_loss, g_loss = model_loss(inputs_real, inputs_z, data_shape[3])
+
+    d_opt, g_opt = model_opt(d_loss, g_loss, learning_rate, beta1)
+
+    steps = 0
+    print_every = 1
+    show_every = 100
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch_i in range(epoch_count):
+            for batch_images in get_batches(batch_size):
+                steps += 1
+
+                # Sample random noise for G
+                batch_z = np.random.uniform(-1, 1, size=(batch_size, z_dim))
+
+                # Run optimizers
+                _ = sess.run(d_opt, feed_dict={inputs_real: batch_images, inputs_z: batch_z})
+                _ = sess.run(g_opt, feed_dict={inputs_z: batch_z, inputs_real: batch_images})
+
+                if steps % print_every == 0:
+                    # At the end of each epoch, get the losses and print them out
+                    train_loss_d = d_loss.eval({inputs_z: batch_z, inputs_real: batch_images})
+                    train_loss_g = g_loss.eval({inputs_z: batch_z})
+
+                    print("Epoch {}/{}...".format(epoch_i+1, epochs),
+                          "Discriminator Loss: {:.4f}...".format(train_loss_d),
+                          "Generator Loss: {:.4f}".format(train_loss_g))
+                    # Save losses to view after training
+                    # losses.append((train_loss_d, train_loss_g))
+
+
+# =================================================
+# Train MNIST
+# =================================================
+
+batch_size = 200
+z_dim = 1
+learning_rate = 0.01
+beta1 = 0.05
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+epochs = 2
+
+mnist_dataset = helper.Dataset('mnist', glob(os.path.join(data_dir, 'mnist/*.jpg')))
+with tf.Graph().as_default():
+    train(epochs, batch_size, z_dim, learning_rate, beta1, mnist_dataset.get_batches,
+          mnist_dataset.shape, mnist_dataset.image_mode)
